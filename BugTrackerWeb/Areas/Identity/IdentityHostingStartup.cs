2cfb4fa7c27e7d03c09c53030249cca86742cd5f
Tomasz Pawlak <Tomasz.Pawlak@historicengland.org.uk>
2021-03-06 11:31:30 +0000

    Identity scaffold added

---
+++ BugTrackerWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BugTrackerWeb.Areas.Identity.Data;
+using BugTrackerWeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BugTrackerWeb.Areas.Identity.IdentityHostingStartup))]
+namespace BugTrackerWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthContextConnection")));
+
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthContext>();
+            });
+        }
+    }
+}
