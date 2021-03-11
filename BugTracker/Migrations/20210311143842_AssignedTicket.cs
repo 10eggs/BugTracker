ccb9c7ed406e6be7810d5481587ae7c616262e59
Tomasz Pawlak <Tomasz.Pawlak@historicengland.org.uk>
2021-03-11 14:47:35 +0000

    Migration and db update for AssignedTicket

---
+++ BugTracker/Migrations/20210311143842_AssignedTicket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BugTracker.Migrations
+{
+    public partial class AssignedTicket : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Updated",
+                table: "Tickets",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Updated",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
