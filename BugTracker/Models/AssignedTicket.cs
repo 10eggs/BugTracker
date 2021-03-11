e75dc28728bbe8b32158090fbfc469094155b75e
Tomasz Pawlak <Tomasz.Pawlak@historicengland.org.uk>
2021-03-11 13:55:28 +0000

    QA and AssignedTicket models have been created

---
+++ BugTracker/Models/AssignedTicket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BugTracker.Models
+{
+    class AssignedTicket:Ticket
+    {
+        public QA Qa { get; set; }
+        public int QaID { get; set; }
+        public string TicketStatus { get; set; }
+        public string Updated { get; set; }
+        public string TicketPriority { get; set; }
+        public string TicketCategory { get; set; }
+
+    }
+}
---
+++ BugTracker/Models/QA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BugTracker.Models
+{
+    class QA
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public List<Project> Projects { get; set; }
+        public ICollection<AssignedTicket> Tickets { get; set; }
+    }
+}
