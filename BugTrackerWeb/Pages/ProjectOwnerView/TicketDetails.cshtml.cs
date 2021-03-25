c301fb9eb7ef96d9bbc1a5c15268b858b1d22bd4
Tomasz Pawlak <Tomasz.Pawlak@historicengland.org.uk>
2021-03-25 02:05:15 +0000

    New form for assigning tickets has been added

--- BugTrackerWeb/Pages/ProjectOwnerView/TicketDetails.cshtml.cs
+++ BugTrackerWeb/Pages/ProjectOwnerView/TicketDetails.cshtml.cs
@@ -31,88 +31,66 @@ namespace BugTrackerWeb.Pages.ProjectOwnerView
         public AssignedTicket AssignedTicket { get; set; }
 
         public SelectList TicketCatDDLOptions { get; set; }
+        public SelectList TicketPriorDDLOptions { get; set; }
+        public SelectList TicketStatDDLOptions { get; set; }
+
+
         public SelectList QAsList { get; set; }
 
         [BindProperty]
         public Project Project { get; set; }
         public Ticket Ticket { get; set; }
-        public string QaName { get; set; }
-
-        //Test properties
-        [BindProperty]
-        public int ProjectId { get; set; }
-
-        [BindProperty]
-        public int TicketId { get; set; }
-
-        //Check this !
-        //[FromQuery(Name = "foo-bar")]
-        //public string FooBar { get; set; }
-
+        
+        //Test property for query parameter
         [FromQuery(Name = "projectId")]
         public string ProjectIdFromQuery { get; set; }
 
         [BindProperty]
         public ICollection<QA> AvailableQAs { get; set; }
 
-        //public IActionResult OnPost()
-        //{
-        //    return RedirectToPage("/ProjectOwnerView/Index");
-        //}
         public void OnGetTicketDetails(int projectId, int ticketId)
         {
             //Check if FromQuery works
             //Checked and works
             Debug.WriteLine($"From query: {ProjectIdFromQuery}");
 
-
-            //This two have been added for test purpose
-            //Works as well
-            //ProjectId = projectId;
-            //TicketId = ticketId;
-            TicketCatDDLOptions = new SelectList(EnumUtil.GetValues<TicketCategory>());
             Project = _pp.Get(projectId);
             Ticket = Project.Tickets
                 .Where(t => t.Id == ticketId)
                 .SingleOrDefault();
 
             AvailableQAs = Project.QAs;
-            QAsList = new SelectList(AvailableQAs.Select(qa=>qa.Name));
+
+
+            QAsList = new SelectList(AvailableQAs.Select(qa => qa.Name));
+            TicketCatDDLOptions = new SelectList(EnumUtil.GetValues<TicketCategory>());
+            TicketPriorDDLOptions = new SelectList(EnumUtil.GetValues<TicketPriority>());
+            TicketStatDDLOptions = new SelectList(EnumUtil.GetValues<TicketStatus>());
         }
-
-        //public IActionResult OnPostCheckAjax(int qaId)
-        //{
-        //    Debug.WriteLine("My name is Luka and qaid is "+qaId);
-        //    return RedirectToPage("/ProjectOwnerView/Index");
-        //}
-
-        public async Task<JsonResult> OnPostCheckAjax(int qaId, int ticketId)
-        {
-            //HTTP stateless, model values are not here
-            //********WEIRD BEHAVIOR - PROJECT ID VALUE DOES NOT AVAILABLE HERE*****************//
-            //Debug.WriteLine("Check if assigned model values are still here.ProjId: " + ProjectId + ", TicketId" + TicketId);
-            //Debug.WriteLine($"Ticket id is {ticketId} and qaId is {qaId}");
-
-
-            _itm.AssignToQa(ticketId, qaId);
-
-            return new JsonResult(new { Success = "HOlA"! });
-            //return RedirectToPage("/ProjectOwnerView/Index");
-
-
-        }
-
         public async Task<JsonResult> OnPostAssignTicket(int qaId, int ticketId)
         {
             _itm.AssignToQa(ticketId, qaId);
-            Debug.WriteLine("Called from handler");
 
-            //previous
+            //Validation here is required, implement it later
+            return new JsonResult(new { success = true, message = "Ticket has been assigned!",errormessage="Something went wrong, try again later"});
+
+            //This Url.Action should be considered as a return type
             //return new JsonResult(new { redirectToUrl = Url.Action("action", "contoller") });
-            //return RedirectToPage("Index");
-            return new JsonResult(new { redirectToUrl = Url.Action("action", "contoller") });
 
-            //return RedirectToPage("/ProjectOwnerView");
+        }
+
+        public async Task<IActionResult> OnPostAssignTicketFromForm()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var ticCat = AssignedTicket.TicketCategory;
+            var ticPriority = AssignedTicket.TicketPriority;
+            var ticStat = AssignedTicket.TicketStatus;
+
+            Debug.WriteLine($"All captured values: {ticCat},{ticPriority},{ticStat}");
+            return Page();
         }
     }
 }
--- BugTrackerWeb/wwwroot/js/poTicketDetails.js
+++ BugTrackerWeb/wwwroot/js/poTicketDetails.js
@@ -10,53 +10,31 @@ $(document).ready(function () {
         console.log('TicketId is  ', ticketId);
         
     }),
-        $('#assignticket').on('click', async function (event) {
-            event.preventDefault();
-            console.log("onClick called!")
-            const response = await $.ajax({
-                type: "POST",
-                url: `/ProjectOwnerView/TicketDetails?handler=CheckAjax`,
-                dataType: 'json',
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader("XSRF-TOKEN",
-                        $('input:hidden[name="__RequestVerificationToken"]').val());
-                },
-                data: { 'qaid': qaId, 'ticketId': ticketId },
-                //success: function (result) {
-                //    console.log('SUCCESS!!!!')
-                //},
-                //error: function (result) {
-                //    console.log('Error is here!')
-                //},
-                //complete: function (result) {
-                //    console.log('Completet!')
-                //}
-                //Weird response here ... ;)
-                //success: function (result) {
-                //    console.log('Success call before checking redirectToUrl')
-                //    if (result.redirectToUrl !== undefined) {
-                //        console.log('Success post call!')
-                //        window.location.replace(result.redirectToUrl);
-                //    }
-                //}
-                //success: function (response) {
-                //    if (respons == true)
-                //        console.log('This is the response: ', response)
-                //    window.location = "https://localhost:44333/ProjectOwnerView";
-                //}
-            }).done(function (response){
-                $('#response').html(response);
-                window.location = 'https://localhost:44333/ProjectOwnerView';
-             })
-
-            //if (response.ok) {
-            //    console.log('HOLA!')
-            //}
-                //.done(function (result) {
-                //    console.log('Ajax call is done!')
-                //    window.location = "https://localhost:44333/ProjectOwnerView"
-                //})
+    $('#assignticket').on('click', async function (event) {
+        event.preventDefault();
+        console.log("onClick called!")
+        const response = await $.ajax({
+            type: "POST",
+            url: `/ProjectOwnerView/TicketDetails?handler=AssignTicket`,
+            dataType: 'json',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: { 'qaid': qaId, 'ticketId': ticketId },
+        }).done(function (response) {
+            $('#assignticket').prop('disabled', true);
+                if (response.success) {
+                    toastr.success(response.message);
+                }
+                else {
+                    toastr.error(response.errormsg);
+                }
         })
+        setTimeout(function () {
+            window.location = "https://localhost:44333/ProjectOwnerView"
+        },3000);
+    })
 })
 
 function getSelectedQa() {
