5ab70bc70cd90ffa317603ac84f57b34239a25d5
Tomasz Pawlak <Tomasz.Pawlak@historicengland.org.uk>
2021-04-14 16:52:42 +0100

    User functionalities have been modularized into separate PageModels

--- BugTracker/Models/Request.cs
+++ BugTracker/Models/Request.cs
@@ -8,11 +8,14 @@ namespace BugTracker.Models
     public class Request
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
         public string Author { get; set; }
         public DateTime CreatedAt { get; set; }
         public Project Project { get; set; }
+        [Required]
         public int ProjectId { get; set; }
 
         public Request()
